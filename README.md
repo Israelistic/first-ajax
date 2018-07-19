57acf2e32122e267c382734384a96e79f3e3fc4d
Israelistic <haguyle@gmail.com>
2018-07-18 22:47:35 -0400

    added answer to README.md, Added JS querys and Ajacx to acomedate the buttons

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 # Your First (Several) Asynchronous Requests
 
 [See assignment in Alexa.](https://alexa.bitmaker.co/wdi/67/assignments/2055/latest)
+1)What HTTP method did your browser use to make the request?
+A) 'GET'
+2)How many milliseconds did it take your browser to complete it?
+A)84ms
+3)What HTTP status code did the server return? What does that mean?
+A)200
+Look at the "Initiator" and "Type" columns.
+A)'other'
+4)Was this an xhr/JavaScript/AJAX request or a normal browser request?
+A)document
+
+
+skelaton AJAX
+$.ajax({
+  url: _____,
+  method: _____,
+  data: _____,
+  dataType: _____
+});
+
+5)
+What are the values in the "Method" and "Status" columns? Compare these values to Step 0.
+
+6)
+What are the values in the "Type" and "Initiator" columns? Compare these values to Step 0.
--- ajax.js
+++ ajax.js
@@ -1,5 +1,95 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  /* Your code goes here */
+  var step1And2 = document.getElementById('b1');
+  step1And2.addEventListener('click', function(e){
+    $.ajax({
+      url:'http://first-ajax-api.herokuapp.com/',
+      method:'GET',
+    }).done(function() {
+        console.log('call sucssesful');
+    })
+  })
+  var step3456 = document.getElementById('pingPongButtom');
+  step3456.addEventListener('click', function(e) {
+    $.ajax({
+      url:'http://first-ajax-api.herokuapp.com/pong',
+      method:'GET',
 
+    }).done(function(responseData) {
+        var step3456Section = document.getElementById("step3456")
+        var  pTag  = document.createElement('p');
+        var textnode = document.createTextNode(responseData);
+          pTag.appendChild(textnode)
+          step3456Section.appendChild(pTag)
+    }).fail(function(responseData) {
+      var step3456Section2 = document.getElementById("step3456")
+      var  ptag2 = document.createElement('p');
+      var  textnode2 = document.createTextNode('something went wrong')
+      ptag2.appendChild(textnode2);
+      step3456Section2.appendChild(ptag2);
+    }).always(function() {
+      console.log('the request is done');
+    })
+  })
+
+  var  countRequest = document.getElementById('countButton');
+  countRequest.addEventListener('click', function(e){
+    $.ajax({
+      url:'http://first-ajax-api.herokuapp.com/count',
+      method:'GET',
+    }).done(function(responseData) {
+      //grab the <element> tag
+      var  step7Section = document.getElementById('step7');
+      //create a tag.
+      var  pTag = document.createElement('p');
+      //translate the response to a text.
+      var  textnode = document.createTextNode(responseData);
+      // append the text to the variable into the <p> tag.
+      pTag.appendChild(textnode);
+      // append the <p> tag into the <section> tag .
+      step7Section.appendChild(pTag);
+    }).fail(function(responseData) {
+      var step7Section = document.getElementById('step7');
+      var pTag = document.createElement('p');
+      var textnode = document.createTextNode('something went wrong');
+      pTag.appendChild(textnode);
+      step7Section.appendChild(pTag);
+    })
+  })
+  var timeButton = document.getElementById('timeButton')
+  timeButton.addEventListener('click', function(e){
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/time',
+      method:'GET',
+      data: {timezone: 'Pacific/Honolulu'},
+      dataType:'text',
+      }).done(function(responseData) {
+      var step8Section = document.getElementById('step8');
+      var pTag = document.createElement('p');
+      var textnode = document.createTextNode(responseData);
+      pTag.appendChild(textnode);
+      step8Section.appendChild(pTag);
+      })
+  })
+    var carButtom = document.getElementById('carButtom');
+    timeButton.addEventListener('click', function(){
+      $.ajax({
+        url:'http://first-ajax-api.herokuapp.com/a_car',
+        method:'GET',
+        dataType: 'html',
+      }).done(function(responseData) {
+        console.log(responseData);
+        var step9Section = document.getElementById('step9');
+        var uList = document.createElement('ul');
+          uList.innerHTML = responseData;
+            uList.setAttribute('id', ' car')
+            step9Section.appendChild(uList)
+      }).fail(function(responseData) {
+        var step9Section = document.getElementById('step9');
+        var uList = document.createElement('ul');
+          uList = document.createTextNode('something went worng');
+          step9Section.appendChild(uList);
+      })
+
+    })
 });
